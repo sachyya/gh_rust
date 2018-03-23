40714d6385b2744d480e485d86eb2514a166de47
sachyya <sachyya1@gmail.com>
2018-03-23 16:08:26 +0545

    initial commit

---
+++ hello_world/target/debug/deps/hello_world-5e1a8ce272ae7966.d
@@ -0,0 +1,5 @@
+/home/themepalace/Desktop/projects/hello_world/target/debug/deps/hello_world-5e1a8ce272ae7966: src/main.rs
+
+/home/themepalace/Desktop/projects/hello_world/target/debug/deps/hello_world-5e1a8ce272ae7966.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/themepalace/Desktop/projects/hello_world/target/debug/hello_world: /home/themepalace/Desktop/projects/hello_world/src/main.rs
